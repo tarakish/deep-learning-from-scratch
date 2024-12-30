fee4ac3d2154415031bfed11a88b35edda842fd2
tarakish <akr.3838soccer14@gmail.com>
2024-12-30 17:26:49 +0900

    NANDメソッドを追加

--- chapter2/perceptron.py
+++ chapter2/perceptron.py
@@ -11,7 +11,28 @@ def AND(x1, x2):
         return 1
 
 
-print(AND(0, 0))
-print(AND(1, 0))
-print(AND(0, 1))
-print(AND(1, 1))
+print("-"*10)
+print("AND method")
+print("AND(0, 0): ", AND(0, 0))
+print("AND(1, 0): ", AND(1, 0))
+print("AND(0, 1): ", AND(0, 1))
+print("AND(1, 1): ", AND(1, 1))
+
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])  # ANDと符号逆転
+    b = 0.7  # ANDと符号逆転
+    tmp = sum(x*w) + b
+    if tmp <= 0:
+        return 0
+    elif tmp > 0:
+        return 1
+
+
+print("-"*10)
+print("NAND method")
+print("NAND(0, 0): ", NAND(0, 0))
+print("NAND(1, 0): ", NAND(1, 0))
+print("NAND(0, 1): ", NAND(0, 1))
+print("NAND(1, 1): ", NAND(1, 1))
