ff05767428437ffb19548115c1df8f5d8411d9a9
tarakish <akr.3838soccer14@gmail.com>
2024-12-30 17:27:00 +0900

    ORメソッドを追加

--- chapter2/perceptron.py
+++ chapter2/perceptron.py
@@ -36,3 +36,22 @@ print("NAND(0, 0): ", NAND(0, 0))
 print("NAND(1, 0): ", NAND(1, 0))
 print("NAND(0, 1): ", NAND(0, 1))
 print("NAND(1, 1): ", NAND(1, 1))
+
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2  # ANDとバイアスだけが異なる
+    tmp = sum(x*w) + b
+    if tmp <= 0:
+        return 0
+    elif tmp > 0:
+        return 1
+
+
+print("-"*10)
+print("OR method")
+print("OR(0, 0): ", OR(0, 0))
+print("OR(1, 0): ", OR(1, 0))  
+print("OR(0, 1): ", OR(0, 1))
+print("OR(1, 1): ", OR(1, 1))
