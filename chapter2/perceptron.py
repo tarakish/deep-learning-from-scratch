6f9f7ab67647eccd7988933a789ed5f1881f47b2
tarakish <akr.3838soccer14@gmail.com>
2024-12-30 17:19:33 +0900

    numpyを使ってリファクタリング

--- chapter2/perceptron.py
+++ chapter2/perceptron.py
@@ -1,10 +1,15 @@
+import numpy as np
+
 def AND(x1, x2):
-    w1, w2, theta = 0.5, 0.5, 0.7
-    tmp = x1*w1 + x2*w2
-    if tmp <= theta:
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = sum(x*w) + b
+    if tmp <= 0:
         return 0
-    elif tmp > theta:
+    elif tmp > 0:
         return 1
+
 
 print(AND(0, 0))
 print(AND(1, 0))
