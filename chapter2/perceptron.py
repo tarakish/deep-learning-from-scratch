7221c63a39c45d7dac19661d774031217f6f2765
tarakish <akr.3838soccer14@gmail.com>
2024-12-30 17:48:32 +0900

    XORメソッドの追加

--- chapter2/perceptron.py
+++ chapter2/perceptron.py
@@ -55,3 +55,18 @@ print("OR(0, 0): ", OR(0, 0))
 print("OR(1, 0): ", OR(1, 0))  
 print("OR(0, 1): ", OR(0, 1))
 print("OR(1, 1): ", OR(1, 1))
+
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
+
+print("-"*10)
+print("XOR method")
+print("XOR(0, 0): ", XOR(0, 0))
+print("XOR(1, 0): ", XOR(1, 0))
+print("XOR(0, 1): ", XOR(0, 1))
+print("XOR(1, 1): ", XOR(1, 1))
